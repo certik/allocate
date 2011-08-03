cca863403e700205eecfe4b03f86493a73c3c1e6
Ondrej Certik <ondrej@certik.cz>
2011-08-02 23:33:22 -0700

    Finish

--- a.f90
+++ a.f90
@@ -4,15 +4,21 @@ implicit none
 
 integer, parameter :: dp=kind(0.d0)
 
-real(dp), allocatable :: A(:)
-
-call test(A)
+do while (.true.)
+    call f()
+end do
 
 contains
+
+subroutine f()
+real(dp), allocatable :: A(:)
+call test(A)
+end subroutine
 
 subroutine test(A)
 real(dp), allocatable, intent(out) :: A(:)
 allocate(A(10000000))
+A(1) = 5
 end subroutine
 
 end program
